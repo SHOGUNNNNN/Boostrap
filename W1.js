a5fbabba83f1142ef1e59ea473fa99449cdc6226
SHOGUNNNNN <st2262@roong-aroon.ac.th>
2024-09-17 15:25:35 +0700

    first commit

---
+++ W1.js
@@ -0,0 +1,23 @@
+let score = -10;
+
+if (score > 100 || score < 0){
+    console.log("Invalid score");
+}else if (score >= 80 && score <= 100){
+    console.log("A");
+} else if (score >= 75){
+    console.log("B+");
+} else if (score >= 70){
+    console.log("B");
+} else if (score >= 65){
+    console.log("C+");
+} else if (score >= 60){
+    console.log("C");
+} else if (score >= 55){
+    console.log("D+");
+} else if (score >= 50){
+    console.log("D");
+} else if (score >= 0){
+    console.log("F");
+} else if (score > 100 || score < 0){
+    console.log("Invalid score");
+}
